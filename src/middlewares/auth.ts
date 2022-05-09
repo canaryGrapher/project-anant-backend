a2c71be7cf6f1e963cc3ce129ac60382a77fed1a
Parthiv-M <parthiv.menon@learner.manipal.edu>
2022-05-10 03:29:17 +0530

    added audience for auth

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -3,7 +3,7 @@ const { auth } = require('express-oauth2-jwt-bearer');
 // Authorization middleware. When used, the Access Token must
 // exist and be verified against the Auth0 JSON Web Key Set.
 const checkJwt = auth({
-  audience: 'undefined',
+  audience: 'http://localhost:3002/', // needs to be changed according to audience name
   issuerBaseURL: process.env.AUTH0_BASE_URL,
 });
 
