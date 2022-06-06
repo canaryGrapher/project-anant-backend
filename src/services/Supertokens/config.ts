7a8a785a23c2bb6fd7a3a42adb78be7089bf4029
Yash Aryan <yasharyan307@outlook.com>
2022-06-06 15:38:08 +0530

    Security updates

--- src/services/Supertokens/config.ts
+++ src/services/Supertokens/config.ts
@@ -9,43 +9,38 @@ export const backendConfig = () => {
     SuperTokens.init({
         framework: "express",
         supertokens: {
-            // These are the connection details of the app you created on supertokens.com
-            connectionURI: "https://00abdf01e24411ecb6cd51056a67bbb0-ap-southeast-1.aws.supertokens.io:3567",
-            apiKey: "R=dinEDm9ehrU7XAc-RBslUy16ddXV",
-            // connectionURI: "https://try.supertokens.com",
+            connectionURI: process.env.SUPERTOKENS_CONNECTION_URI,
+            apiKey: process.env.SUPERTOKENS_API_KEY,
         },
         appInfo,
         recipeList: [
             ThirdParty.init({
                 signInAndUpFeature: {
                     providers: [
-                        // We have provided you with development keys which you can use for testing.
-                        // IMPORTANT: Please replace them with your own OAuth keys for production use.
                         Google({
-                            clientId: "1060725074195-kmeum4crr01uirfl2op9kd5acmi9jutn.apps.googleusercontent.com",
-                            clientSecret: "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"
+                            clientId: process.env.SUPERTOKENS_SOCIAL_GOOGLE_ID,
+                            clientSecret: process.env.SUPERTOKENS_SOCIAL_GOOGLE_SECRET,
                         }),
                         Github({
-                            clientId: "467101b197249757c71f",
-                            clientSecret: "e97051221f4b6426e8fe8d51486396703012f5bd"
+                            clientId: process.env.SUPERTOKENS_SOCIAL_GITHUB_ID,
+                            clientSecret: process.env.SUPERTOKENS_SOCIAL_GITHUB_SECRET
                         }),
                         Apple({
-                            clientId: "4398792-io.supertokens.example.service",
+                            clientId: process.env.SUPERTOKENS_SOCIAL_APPLE_ID,
                             clientSecret: {
-                                keyId: "7M48Y4RYDL",
-                                privateKey:
-                                    "-----BEGIN PRIVATE KEY-----\nMIGTAgEAMBMGByqGSM49AgEGCCqGSM49AwEHBHkwdwIBAQQgu8gXs+XYkqXD6Ala9Sf/iJXzhbwcoG5dMh1OonpdJUmgCgYIKoZIzj0DAQehRANCAASfrvlFbFCYqn3I2zeknYXLwtH30JuOKestDbSfZYxZNMqhF/OzdZFTV0zc5u5s3eN+oCWbnvl0hM+9IW0UlkdA\n-----END PRIVATE KEY-----",
-                                teamId: "YWQCXGJRJL",
+                                keyId: process.env.SUPERTOKENS_SOCIAL_APPLE_SECRET_ID,
+                                privateKey: process.env.SUPERTOKENS_SOCIAL_APPLE_PRIVATE_KEY,
+                                teamId: process.env.SUPERTOKENS_SOCIAL_APPLE_TEAM_ID,
                             },
                         }),
                         Facebook({
-                            clientSecret: "FACEBOOK_CLIENT_SECRET",
-                            clientId: "FACEBOOK_CLIENT_ID"
+                            clientSecret: process.env.SUPERTOKENS_SOCIAL_FACEBOOK_ID,
+                            clientId: process.env.SUPERTOKENS_SOCIAL_FACEBOOK_SECRET,
                         })
                     ]
                 }
             }),
-            Session.init() // initializes session features
+            Session.init()
         ]
     });
 }
